92bc998261bf93f4919c997220b22d23a3a05e2a
Brian7950 <brianh7950@gmail.com>
2022-03-21 13:17:50 -0600

    dynamic repo information in api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,14 @@
-var GetUserRepos = function(){
-    console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos");
+var GetUserRepos = function(user){
+    //format the github api url 
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
 };
 
 
-GetUserRepos();
+GetUserRepos("microsoft"); //test
